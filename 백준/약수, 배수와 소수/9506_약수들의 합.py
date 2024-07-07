7d4c6fe7886c79cd84bd58eec624ecb016e0fac2
dygks4713 <yohan9098@gmail.com>
2024-07-08 08:27:54 +0900

    add comment 9506_약수드의 합.py file

--- 백준/약수, 배수와 소수/9506_약수들의 합.py
+++ 백준/약수, 배수와 소수/9506_약수들의 합.py
@@ -3,15 +3,15 @@ result = 0
 while True:
     st = []
     n = int(input())
-    if n == -1:
+    if n == -1: # -1 입력 받으면 프로그램 종료
         break
 
     for i in range(1, n):
-        if n%i == 0:
-            st.append(i)
+        if n%i == 0: # n의 약수
+            st.append(i) # n의 약수를 n의 리스트에 추가
         
     if sum(st) == n:
         print(n, "=", end=" ")
-        print(*st, sep="+") # 리스트나 튜플에 있는 요소를 함수에 개별 인자로 전달할 때 *를 사용
+        print(*st, sep=" + ") # 리스트나 튜플에 있는 요소를 함수에 개별 인자로 전달할 때 *를 사용
     else:
         print("%d is NOT perfect." %n)
